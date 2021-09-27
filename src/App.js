731a4ef003d7ad405746ea1fbfc5dfd81fc5ef23
Larry Allen <l.allen@gobigwheel.com>
2021-09-27 14:53:11 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <FormControlLabel 
+control ={<Checkbox
+      checked={checked}
+      icon={<DeleteIcon />}
+      checkedIcon={<SaveIcon />}
+      onChange={(e)=>setChecked(e.target.checked)}
+      inputProps={{
+        'aria-label': 'secondary checkbox'
+      }}
+      />}
+      label="Testing Checkbox"
+      />
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
+        <ButtonGroup size="large" variant="contained" color="primary">
+          <Button startIcon={<SaveIcon />}>
+            Save
+          </Button>
+          <Button endIcon={<DeleteIcon />}>
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
